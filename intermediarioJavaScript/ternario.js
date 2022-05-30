9b19ef8d87e3f363fe5f5287eab9a406170170bd
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-05-30 14:53:17 -0300

    Add Operador Ternário e Arrow Functions

---
+++ intermediarioJavaScript/ternario.js
@@ -0,0 +1,21 @@
+let isValid = false;
+
+//sem ternario
+function verify(isValid) {
+  if (isValid) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(verify(isValid));
+
+//Com ternário
+const result = isValid ? true : false;
+
+let zero = 0;
+
+const numeroResultado = zero == 0 ? 0 : -1;
+
+console.log(numeroResultado);
