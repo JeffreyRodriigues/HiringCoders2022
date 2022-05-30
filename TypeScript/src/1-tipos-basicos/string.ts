6cd3d29c3c269bc40d0c86e9a366531dd475c9cb
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-05-30 17:57:33 -0300

    add Basic about Typescript

---
+++ TypeScript/src/1-tipos-basicos/string.ts
@@ -0,0 +1,11 @@
+let nome: string;
+
+// ...
+
+nome = 'Jeffrey';
+
+function nomeCompleto(nome: string, sobrenome: string) {
+  return `${nome} ${sobrenome}`
+}
+
+nomeCompleto('Jeffrey', 'Rodrigues');
